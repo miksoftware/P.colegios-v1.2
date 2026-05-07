20a56de3a7e98a8c7ec64dd37c4ff66afd96a6c8
Marileidy afanador <mafanadord@uts.edu.co>
2026-05-06 20:28:38 -0500

    new tamaño report

--- resources/views/livewire/payment-report-management.blade.php
+++ resources/views/livewire/payment-report-management.blade.php
@@ -1,5 +1,5 @@
 ﻿<div class="min-h-screen bg-gray-50 py-8">
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div class="max-w-full mx-auto px-4 sm:px-6 lg:px-8">
         {{-- Header --}}
         <div class="flex flex-col sm:flex-row justify-between items-start sm:items-center mb-8 gap-4">
             <div>

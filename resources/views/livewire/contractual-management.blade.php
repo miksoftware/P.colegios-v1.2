699e240c7506c1c2be82feb8a49e00d91bc5f58f
Daniel Chavarro <softwaremik@gmail.com>
2026-08-06 17:30:26 -0500

    cambio de ancho en los modulos de presupuestos

--- resources/views/livewire/contractual-management.blade.php
+++ resources/views/livewire/contractual-management.blade.php
@@ -1,5 +1,5 @@
 <div class="min-h-screen bg-gray-50 py-8">
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div class="max-w-full mx-auto px-4 sm:px-6 lg:px-8">
 
         @if($currentView === 'list')
             {{-- ==================== VISTA LISTADO ==================== --}}
